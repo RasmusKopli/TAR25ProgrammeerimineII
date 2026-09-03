7b7e87df51b1509261701aa3a47b0b23431ea423
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 13:42:53 +0300

    Created new project and added class libraries, and a Context file in the data library

---
+++ ShopTARpe25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTARpe25.Data
+{
+    public class ShopTARpe25Context : DbContext
+    {
+        public ShopTARpe25Context(DbContextOptions<ShopTARpe25Context> options) : base(options)
+        {
+
+        }
+    }
+}
