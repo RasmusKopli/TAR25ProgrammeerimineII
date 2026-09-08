5853db2f1a3016d0b2bc07914d8d128ab37805aa
RasmusKopli <kopli.rasmus@gmail.com>
2026-09-08 17:54:18 +0300

    CreateView, IndexView, Create and Index ViewModels for Spaceship, Interface for SpaceshipServices

--- ShopTARpe25/ShopTARpe25.ApplicationServices/Services/SpaceshipServices.cs
+++ ShopTARpe25/ShopTARpe25.ApplicationServices/Services/SpaceshipServices.cs
@@ -1,10 +1,11 @@
 ﻿using ShopTARpe25.Core.Domain;
 using ShopTARpe25.Core.Dto;
 using ShopTARpe25.Data;
+using ShopTARpe25.Core.ServiceInterface;
 
 namespace ShopTARpe25.ApplicationServices.Services
 {
-    public class SpaceshipServices
+    public class SpaceshipServices : ISpaceshipServices
     {
         private readonly ShopTARpe25Context _context;
 
--- ShopTARpe25/ShopTARpe25/Controllers/SpaceshipController.cs
+++ ShopTARpe25/ShopTARpe25/Controllers/SpaceshipController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARpe25.Models.Spaceship;
 
 namespace ShopTARpe25.Controllers
 {
@@ -7,6 +8,22 @@ namespace ShopTARpe25.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        //Kui kasutaja klikib "Create" nuppu, siis see meetod käivitatakse
+        //tagastab kasutajale vormi, kuhu saab sisestada andmed
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Kui oled teinud vormi, siis see meetod käivitatakse
+        //saadab andmed serverisse, kus need salvestatakse andmebaasi
+        [HttpPost]
+        public async Task<IActionResult> Create(SpaceshipCreateViewModel vm)
+        {
+            return RedirectToAction(nameof(Index));
         }
     }
 }
