de3e9f8f1a7dd4bb669167b5073a0506990a0af2
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 14:12:35 +0300

    SQL connection, constructor in SpaceshipController and builder in program class to make create work

--- ShopTARpe25/ShopTARpe25/Program.cs
+++ ShopTARpe25/ShopTARpe25/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ShopTARpe25.ApplicationServices.Services;
+using ShopTARpe25.Controllers;
+using ShopTARpe25.Core.ServiceInterface;
+using ShopTARpe25.Data;
+
 namespace ShopTARpe25
 {
     public class Program
@@ -8,6 +14,14 @@ namespace ShopTARpe25
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+
+            //selleks, et tuleb installida Microsoft.EntityFrameworkCore.SqlSever
+            //ja Microsoft.EntityFrameworkCore.Tools NuGet paketid
+            //kui installitud, siis viidata namespacesis Microsoft.EntityFrameworkCore
+            builder.Services.AddDbContext<ShopTARpe25Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
