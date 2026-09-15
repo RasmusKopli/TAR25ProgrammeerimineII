1c689e60b48aff0d03b54fcafa9605ff05adf0f2
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-15 14:12:20 +0300

    Spaceship index view, removed migration that caused issue

--- ShopTARpe25/ShopTARpe25/Controllers/SpaceshipController.cs
+++ ShopTARpe25/ShopTARpe25/Controllers/SpaceshipController.cs
@@ -2,6 +2,7 @@
 using ShopTARpe25.Models.Spaceship;
 using ShopTARpe25.Core.Dto;
 using ShopTARpe25.Core.ServiceInterface;
+using ShopTARpe25.Data;
 
 
 namespace ShopTARpe25.Controllers
@@ -9,21 +10,38 @@ namespace ShopTARpe25.Controllers
     public class SpaceshipController : Controller
     {
         private readonly ISpaceshipServices _spaceshipService;
+        private readonly ShopTARpe25Context _context;
 
         //teha constructor et saaks kasutada teenust, mis on
         //defineeritud ISpaceshipServices liideses
+        //lisage Context
         public SpaceshipController
             (
-                ISpaceshipServices spaceshipService
+                ISpaceshipServices spaceshipService,
+                ShopTARpe25Context context
             )
         {
             _spaceshipService = spaceshipService;
+            _context = context;
         }
 
 
         public IActionResult Index()
         {
-            return View();
+            //loome vaheinstantsi domaini ja ViewModeli vahel
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Classification = x.Classification,
+                    BuiltDate = x.BuiltDate,
+                    Crew = x.Crew,
+                    EnginePower = x.EnginePower
+                });
+
+
+            return View(result);
         }
 
         //kui kasutaja klikib "Create" nuppu, siis see meetod käivitatakse
